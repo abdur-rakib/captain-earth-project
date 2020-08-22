a2fc99f4542b961a124c12d6a4e607c3650cec64
Abdur Rakib <43021476+abdur-rakib@users.noreply.github.com>
2020-08-22 13:02:31 +0600

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles/App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LeaderBoard from "./pages/LeaderBoard";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Main App</h1>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/leaderboard" component={LeaderBoard} />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LeaderBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeaderBoard = () => {
+  return (
+    <div>
+      <h1>LeaderBoard</h1>
+    </div>
+  );
+};
+
+export default LeaderBoard;
