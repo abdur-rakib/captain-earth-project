db04b93a0eed8c9291ae66e6e5aa09a00f7b6db1
Abdur Rakib <43021476+abdur-rakib@users.noreply.github.com>
2020-08-22 13:07:43 +0600

    added tiers and motives static data

---
+++ src/data/motives.js
@@ -0,0 +1,20 @@
+export const motives = [
+  {
+    id: 1,
+    title: "COMPETITIVE PLATFORM",
+    description:
+      "Captain Earth a competitive platform to inspire and facilitate humane behaviour and grow positive mentality in young generation.",
+  },
+  {
+    id: 2,
+    title: "GROW SYMPATHY",
+    description:
+      "The aim of this game is to grow empathy and respect towards frontline fighters(Doctors/other health workers/polices)against corona virus.",
+  },
+  {
+    id: 3,
+    title: "TEAR DOWN HATRED",
+    description:
+      "We want to break the bad blood and negligence toward corona patients and their families and make their life easier by helping them.",
+  },
+];
