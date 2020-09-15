161876c14b78bf18c1488546f1ebde1bb74de2dc
abdur-rakib <abdurrakib961@gmail.com>
2020-09-16 00:10:35 +0600

    Added style of answerDetails

--- src/components/NewsFeed/AnswerDetails.js
+++ src/components/NewsFeed/AnswerDetails.js
@@ -3,14 +3,15 @@ import { useParams, Link } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import { db } from "../../firebase/util";
-import dayjs from "dayjs";
+// import dayjs from "dayjs";
 import Spinner from "../../utils/Spinner";
 import "./SinglePost.css";
 import Navigation from "../Navigation/Navigation";
 import maskot from "../../styles/img/maskot.png";
-import person from "../../styles/img/person.png";
-import Video from "./Video";
+// import person from "../../styles/img/person.png";
+// import Video from "./Video";
 import Footer from "../Footer/Footer";
+import SinglePost from "./SinglePost";
 
 const AnswerDetails = () => {
   const { answerRef } = useParams();
@@ -20,56 +21,55 @@ const AnswerDetails = () => {
     db.doc(`/answers/${answerRef}`)
       .get()
       .then((doc) => {
-        setAnswer(doc.data());
+        setAnswer({ ...doc.data(), ref: doc.id });
       });
 
     // eslint-disable-next-line
   }, []);
-  console.log(answer);
-  const renderPostDetail = answer ? (
-    <main className="postdetail">
-      <div className="postdetail__video">
-        {/* <video width="100%" controls>
-          <source src={answer?.url} type="video/mp4" />
-        </video> */}
-        <Video url={answer?.url} />
-      </div>
+  // const renderPostDetail = answer ? (
+  //   <main className="postdetail">
+  //     <div className="postdetail__video">
+  //       {/* <video width="100%" controls>
+  //         <source src={answer?.url} type="video/mp4" />
+  //       </video> */}
+  //       <Video url={answer?.url} />
+  //     </div>
 
-      <div className="postdetail__doc">
-        <div className="doc">
-          {/* <h1>{title}</h1> */}
-          <div className="user">
-            <div className="post__user__image">
-              {answer?.userImage === "" ? (
-                <img src={person} alt="userpic" />
-              ) : (
-                <img src={answer?.userImage} alt="userpic" />
-              )}
-            </div>
-            <div className="post__user__info">
-              <h2 className="name">{answer?.userName}</h2>
-              <p style={{ textTransform: "uppercase" }}>
-                {dayjs(answer?.createdAt).format("D MMM h:mm A")}
-              </p>
-            </div>
-          </div>
-          <div className="doc__caption">
-            <p>{answer?.body}</p>
-          </div>
-        </div>
-        <div className="responses">
-          <div className="response response__btn">
-            <span className="response__name">
-              <i className="far fa-heart"></i>
-            </span>
-            <span className="response__count">{answer?.likeCount}</span>
-          </div>
-        </div>
-      </div>
-    </main>
-  ) : (
-    <Spinner />
-  );
+  //     <div className="postdetail__doc">
+  //       <div className="doc">
+  //         {/* <h1>{title}</h1> */}
+  //         <div className="user">
+  //           <div className="post__user__image">
+  //             {answer?.userImage === "" ? (
+  //               <img src={person} alt="userpic" />
+  //             ) : (
+  //               <img src={answer?.userImage} alt="userpic" />
+  //             )}
+  //           </div>
+  //           <div className="post__user__info">
+  //             <h2 className="name">{answer?.userName}</h2>
+  //             <p style={{ textTransform: "uppercase" }}>
+  //               {dayjs(answer?.createdAt).format("D MMM h:mm A")}
+  //             </p>
+  //           </div>
+  //         </div>
+  //         <div className="doc__caption">
+  //           <p>{answer?.body}</p>
+  //         </div>
+  //       </div>
+  //       <div className="responses">
+  //         <div className="response response__btn">
+  //           <span className="response__name">
+  //             <i className="far fa-heart"></i>
+  //           </span>
+  //           <span className="response__count">{answer?.likeCount}</span>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </main>
+  // ) : (
+  //   <Spinner />
+  // );
   return (
     <>
       <Navigation />
@@ -78,7 +78,18 @@ const AnswerDetails = () => {
           <img src={maskot} alt="Logo" className="newsfeed__logo" />
         </Link>
       </header>
-      {renderPostDetail}
+      {answer ? (
+        <div
+          className="feed__feedbar profile__feed"
+          style={{ margin: "auto", width: "60%" }}
+        >
+          <div className="feed__feedbar__posts profile__feed__posts">
+            <SinglePost key={answer.ref} answer={answer} />
+          </div>
+        </div>
+      ) : (
+        <Spinner />
+      )}
       <Footer />
     </>
   );
